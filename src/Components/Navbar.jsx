44014d67efefefdf45ade42285db25592491bde6
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-07 07:28:20 +0600

    top nav part added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import { BsCart2 } from "react-icons/bs";
+import { CiHeart } from "react-icons/ci";
+import { FiPhoneCall } from "react-icons/fi";
+import { GoPerson } from "react-icons/go";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <>
-    
+      <nav>
+        {/* ------------------top nav part--------- */}
+        <div className="topNav bg-purple h-11">
+          <div className="container">
+            <div className="item flex items-center justify-between py-3">
+              <div className="left-item flex items-center gap-12">
+                <div className="flex items-center gap-3 text-base text-whiteText font-semibold font-josefin">
+                  <MdOutlineEmail />
+                  examplw@gmail.com
+                </div>
+                <div className="flex items-center gap-3 text-base text-whiteText font-semibold font-josefin">
+                  <FiPhoneCall />
+                  (12345)67890
+                </div>
+              </div>
+              <div className="right-item flex items-center gap-4">
+                <div className="flex items-center gap-1 text-base text-whiteText font-semibold font-josefin">
+                  Login
+                  <GoPerson />
+                </div>
+                <div className="flex items-center gap-1 text-base text-whiteText font-semibold font-josefin">
+                  Wishlist
+                  <CiHeart />
+                </div>
+                <div className="text-base text-whiteText font-semibold font-josefin">
+                  <BsCart2 />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div className='w-full h-screen bg-amber-600'>
+    <div>
 
     </div>
     
