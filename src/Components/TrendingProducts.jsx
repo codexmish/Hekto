a4f803c937a08ef7eabddc747e92296b2efd725f
codexmish <majharulislam.shanto.70@gmail.com>
2025-10-19 18:54:27 +0600

    tranding section added

--- src/Components/TrendingProducts.jsx
+++ src/Components/TrendingProducts.jsx
@@ -4,11 +4,7 @@ import CommonTendingProducts from "./common/CommonTendingProducts";
 import axios from "axios";
 
 const TrendingProducts = () => {
-
-
-
-
-    const [trendongProduct, setTrendingProduct] = useState();
+  const [trendongProduct, setTrendingProduct] = useState();
 
   useEffect(() => {
     axios
@@ -17,30 +13,16 @@ const TrendingProducts = () => {
       .catch((err) => console.log(err));
   }, []);
 
-
-
-
-
-
-
   return (
     <>
       <section className="mt-[132px] mb-[136px]">
         <CommonSectionHead head={"Trending Products"} />
 
         <div className="container">
-
           <div className="mt-8 flex flex-wrap items-center justify-center lg:justify-between px-3 lg:px-0">
             {trendongProduct?.slice(3, 7).map((item) => (
               <div key={item.id} className="mt-5 lg:mt-0">
-                <CommonTendingProducts img={item.thumbnail}/>
-                {/* <CommonFeaturedProduct
-                  img={item.thumbnail}
-                  title={item.title}
-                  code={item.meta.barcode}
-                  price={item.price}
-                /> */}
-
+                <CommonTendingProducts img={item.thumbnail} head={item.title} discountPrice={Math.round(item.price-((item.price*item.discountPercentage)/100)) } price={item.price}/>
               </div>
             ))}
           </div>
--- src/Components/common/CommonTendingProducts.jsx
+++ src/Components/common/CommonTendingProducts.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const CommonTendingProducts = (img, head, discountPrice, price) => {
+const CommonTendingProducts = ({img, head, discountPrice, price}) => {
   return (
     <>
-      <div className="w-[270px] h-[350px] p-3">
+      <div className="w-[270px] h-[350px] p-3 bg-gray-50">
         <div className="w-[247px] h-[244px] bg-gray-200 flex items-center justify-center">
           <div className="img">
             <img src={img} alt="product" />
@@ -13,8 +13,8 @@ const CommonTendingProducts = (img, head, discountPrice, price) => {
         <div className="content mt-4">
             <h2 className="text-base text-navyBlue font-bold font-lato">{head}</h2>
             <div className="price flex items-center gap-3 mt-2">
-                <p className="text-sm text-navyBlue font-normal font-josefin">{discountPrice} </p>
-                <p className="text-xs text-gray-300 font-normal font-josefin">{price} </p>
+                <p className="text-sm text-navyBlue font-normal font-josefin">${discountPrice} </p>
+                <p className="text-xs text-gray-300 font-normal font-josefin line-through">${price} </p>
             </div>
         </div>
       </div>
